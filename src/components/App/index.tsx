791cf1b8475faf5a0627e709f028b455e55d9fb7
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-05-21 20:33:47 +0200

    lint settings

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import prettier from 'prettier'
+const App: React.FC = () => {
+  const ufuk = ''
+  console.log('denen')
+  return <div>Ana component</div>
+}
+
+export default App
