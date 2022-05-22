7011aeb0e725be379f8963b4393410a6a66e92f5
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-05-22 16:19:36 +0200

    chakra set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider resetCSS>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
