d5f22222cd5819c091aaf04a9d221eae150e421b
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-05-26 18:58:44 +0200

    formik chakra setup

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from 'src/pages/Home'
+import RegisterPage from 'src/pages/RegisterPage'
 import webRoutes from 'src/utils/web-routers'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path={webRoutes.homepage} element={<Home />} />
+        <Route path={webRoutes.register} element={<RegisterPage />} />
       </Routes>
     </Router>
   )
---
+++ src/components/form-fields/FormInput.tsx
@@ -0,0 +1,109 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-props-no-spreading */
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from '@chakra-ui/form-control'
+import { Input, InputGroup } from '@chakra-ui/input'
+import { Text, Textarea } from '@chakra-ui/react'
+import { useField } from 'formik'
+import React, { ChangeEvent, PropsWithChildren, ReactElement } from 'react'
+import ReactInputMask, { Props as ReactInputMaskProps } from 'react-input-mask'
+import { CustomInputProps, CustomTextareaProps } from 'src/utils/type-helpers'
+
+type FormInputProps = {
+  label?: string
+  name: string
+  extraInfo?: string | React.ReactNode
+  innerRef?: React.RefObject<HTMLInputElement | HTMLTextAreaElement>
+  customErrorMessage?: boolean
+  inputType?: 'input' | 'textarea'
+}
+
+function FormInput(
+  props: PropsWithChildren<
+    FormInputProps &
+      Partial<ReactInputMaskProps> &
+      (CustomInputProps | CustomTextareaProps)
+  >
+): ReactElement {
+  const {
+    label,
+    name,
+    customErrorMessage,
+    inputType = 'input',
+    isDisabled,
+    isReadOnly,
+    isRequired,
+    extraInfo,
+    ...otherProps
+  } = props
+
+  const [field, meta, helpers] = useField(name)
+  const { setValue } = helpers
+
+  const hasErrors = !!(meta.touched && meta.error)
+
+  let error: string | unknown = meta.error || ''
+  const errorIsString = typeof error === 'string'
+
+  if (errorIsString) {
+    error = meta.error as string
+  } else {
+    error = meta.error as unknown
+  }
+
+  const handleOnChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <FormControl
+      mb='6'
+      isInvalid={hasErrors}
+      isDisabled={isDisabled}
+      isReadOnly={isReadOnly}
+      isRequired={isRequired}
+    >
+      {label && <FormLabel htmlFor={field.name}>{label}</FormLabel>}
+
+      <InputGroup>
+        {inputType === 'input' ? (
+          <Input
+            as={otherProps.mask ? ReactInputMask : undefined}
+            value={field.value || ''}
+            onChange={handleOnChange}
+            {...(otherProps as CustomInputProps)}
+            {...field}
+            id={field.name}
+          />
+        ) : (
+          <Textarea
+            {...field}
+            value={field.value || ''}
+            onChange={handleOnChange}
+            {...(otherProps as CustomTextareaProps)}
+            id={field.name}
+          />
+        )}
+      </InputGroup>
+
+      {extraInfo && (
+        <Text size='small' color='gray.500' mt='2'>
+          {extraInfo}
+        </Text>
+      )}
+
+      <FormErrorMessage>
+        {error !== undefined &&
+          (customErrorMessage ? error : typeof error === 'string' ? error : '')}
+      </FormErrorMessage>
+    </FormControl>
+  )
+}
+
+export default FormInput
