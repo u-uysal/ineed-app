a3a59aecdba90f5558ee12f3944398ac12976c3a
Ufuk UYSAL <ufuk.uysal@oakfields.io>
2022-06-17 23:02:51 +0200

    add formik validation fields

---
+++ src/interfaces/login.interface.ts
@@ -0,0 +1,6 @@
+export class Login {
+  email = ''
+
+  password = ''
+}
+export default Login
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,66 +3,107 @@
 import {
   Flex,
   Box,
-  FormControl,
-  FormLabel,
-  Input,
-  Checkbox,
   Stack,
   Link,
   Button,
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+} from '@chakra-ui/react'
+import { Form, Formik } from 'formik'
+import FormInput from 'src/components/form-fields/FormInput'
+import Login from 'src/interfaces/login.interface'
+import * as Yup from 'yup'
 
 export default function LoginPage() {
+  const loginSchema: Yup.SchemaOf<Login> = Yup.object({
+    // use regex for email validation
+    // TODO servet
+    email: Yup.string().email().required(),
+    // Bir buyuk harf bir sembol(en az) ve en az 6 karakter
+    // validationda bu hatayi goster
+    password: Yup.string().required(),
+  }).defined()
+
+  const submitHandler = (input: Login) => {
+    // backend baglaninca
+    // eslint-disable-next-line no-unused-vars
+    const randomData = input
+  }
+
   return (
-    <Flex
-      minH='100vh'
-      align='center'
-      justify='center'
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
-        <Stack align='center'>
-          <Heading fontSize='4xl'>Sign in to your account</Heading>
-          <Text fontSize='lg' color='gray.600'>
-            to enjoy all of our cool <Link color='blue.400'>features</Link> ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded='lg'
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow='lg'
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align='start'
-                justify='space-between'>
-                <Checkbox>Remember me</Checkbox>
-                <Link color='blue.400'>Forgot password?</Link>
+    <Formik
+      initialValues={new Login()}
+      // TODO onsubmit icin UseMutation Hookunu kullan (en son)
+      onSubmit={(values) => submitHandler(values)}
+      validationSchema={loginSchema}
+    >
+      {() => (
+        <Form>
+          <Flex
+            minH='100vh'
+            align='center'
+            justify='center'
+            bg={useColorModeValue('gray.50', 'gray.800')}
+          >
+            <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
+              <Stack align='center'>
+                <Heading fontSize='4xl'>Sign in to your account</Heading>
+                <Text fontSize='lg' color='gray.600'>
+                  to enjoy all of our cool{' '}
+                  <Link color='blue.400'>features</Link> ✌️
+                </Text>
               </Stack>
-              <Button
-                bg='blue.400'
-                color='white'
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign in
-              </Button>
+              <Box
+                rounded='lg'
+                bg={useColorModeValue('white', 'gray.700')}
+                boxShadow='lg'
+                p={8}
+              >
+                <Stack spacing={4}>
+                  <FormInput
+                    name='email'
+                    label='Email'
+                    placeholder='Your email adress'
+                  />
+                  <FormInput
+                    name='password'
+                    label='Password'
+                    placeholder='Password'
+                    // show password
+                    // TODO SERVET
+                    type='password'
+                  />
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: 'column', sm: 'row' }}
+                      align='start'
+                      justify='space-between'
+                    >
+                      <Link color='blue.400'>Forgot password?</Link>
+                    </Stack>
+                    <Button
+                      bg='blue.400'
+                      color='white'
+                      _hover={{
+                        bg: 'blue.500',
+                      }}
+                    >
+                      Sign In
+                    </Button>
+                    {/* TODO
+                    
+                    Eger kullanicinin hic hesabi yoksa register page'e yonlendir.
+                    
+                    
+                    */}
+                  </Stack>
+                </Stack>
+              </Box>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-  );
+          </Flex>
+        </Form>
+      )}
+    </Formik>
+  )
 }
